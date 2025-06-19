929c1691f0ea76c16d14989e84d7edc62d0ea61a
Emilio <1172386+emiliodeg@users.noreply.github.com>
2025-06-19 16:59:18 -0300

    added keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: true,
   supabase: {
-    databaseUrl: process.env['SUPABASE_DATABASE_URL'] || '',
-    anonKey: process.env['SUPABASE_ANON_KEY'] || '',
+    databaseUrl:
+      process.env['SUPABASE_DATABASE_URL'] ||
+      'https://jtyehfsfjlasqwxddjss.supabase.co',
+    anonKey:
+      process.env['SUPABASE_ANON_KEY'] ||
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0eWVoZnNmamxhc3F3eGRkanNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzNDg2NTAsImV4cCI6MjA2NTkyNDY1MH0.OO0dx8WW0JKTJCh4kM-N_xW-r7JoYGUlHCU6p5RX9GE',
   },
 };
